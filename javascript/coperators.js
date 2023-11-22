2fc4f2502a7a12ce4ff7c0b2d036253b12c8dc27
nsandeepkumar25 <sandeepkumar.n25@gmail.com>
2023-11-22 19:26:16 +0530

    pushing comparision operators to git hub

---
+++ javascript/coperators.js
@@ -0,0 +1,23 @@
+// let x=6;
+// let y=4;
+// console.log(x=y);
+
+// let x=6;
+// let y=4;
+// console.log(x==y);
+
+// let x=6;
+// let y="6";
+// console.log(x===y);
+
+// let x=6;
+// let y=4;
+// console.log(x!=y);
+
+// let x=6;
+// let y=4;
+// console.log(x<y);
+
+let x=6;
+let y=4;
+console.log(x>y);
