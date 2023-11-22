71d591be056353eb3461b107af66a323e3f8e6d6
nsandeepkumar25 <sandeepkumar.n25@gmail.com>
2023-11-22 19:28:07 +0530

    pushing basic operators and javascript variable types

---
+++ javascript/test1.js
@@ -0,0 +1,42 @@
+// console.log("hello sandeep")
+// let x,y,z
+// x=6,y=1
+// z=x-y
+// console.log(z)
+// console.log(typeof z)
+// datatype string
+// let name="sandeep"
+// console.log(name)
+// console.log(typeof name)
+// assining values
+// let x=5
+// let y=1
+// z=x+y;
+// let x = 5;
+// let y = 2;
+// let z = x * y;
+// console.log(z)
+// let x = 5;
+// let y = 2;
+// let z = x + y;
+// console.log(z)
+// let x=4
+// let y=3
+// let z=x%y
+// console.log(z)
+// let x=5
+// x++
+// let z=x
+// console.log(z)
+// let x = 5;
+// x--;
+// let z = x;
+// console.log(z)
+// let x = 5;
+// let z = x ** 2;
+// console.log(z)
+let x=4
+let y="6"
+let z=x+y
+console.log(z)
+
---
+++ javascript/vtypes.js
@@ -0,0 +1,19 @@
+//difference between java script variables
+//var   let   const
+// var x=9
+// console.log("first", x)
+
+// var x=11
+// console.log("second",x)
+
+// let x=9
+// console.log("first", x)
+
+//  x=11
+// console.log("second",x) 
+
+const x=9
+console.log("first", x)
+
+  x=11
+console.log("second",y)
